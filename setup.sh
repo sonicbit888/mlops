26482dc7e8ba396257f38222a3e1a61aabfd43e7
sonicbit888 <53375679+sonicbit888@users.noreply.github.com>
2024-09-22 17:25:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="testml"
+GROUP="Bern_AZUREML_Resource"
 LOCATION="eastus"
-WORKSPACE="azureml"
+WORKSPACE="AZURE_ML"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
